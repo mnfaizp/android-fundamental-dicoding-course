bc8d245e4e61d7d7fbd902202b5c972462f4b223
Znth3 <zynthkas@gmail.com>
2021-03-14 17:17:08 +0700

    Intent with result feedback

--- app/src/main/java/com/example/fundamental/IntentActivity.kt
+++ app/src/main/java/com/example/fundamental/IntentActivity.kt
@@ -1,18 +1,34 @@
 package com.example.fundamental
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 
 class IntentActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var tvResult: TextView
+
+    companion object {
+        private const val REQUEST_CODE = 100
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intent)
 
         val btnMoveWithString: Button = findViewById(R.id.btn_move_with_string)
         btnMoveWithString.setOnClickListener(this)
+
+        val btnMoveWithObject: Button = findViewById(R.id.btn_move_with_object)
+        btnMoveWithObject.setOnClickListener(this)
+
+        val btnMoveWithResult: Button = findViewById(R.id.btn_move_with_result)
+        btnMoveWithResult.setOnClickListener(this)
+
+        tvResult = findViewById(R.id.tv_received_result)
     }
 
     override fun onClick(v: View?) {
@@ -37,7 +53,24 @@ class IntentActivity : AppCompatActivity(), View.OnClickListener {
                 intentWithObject.putExtra(IntentWithObjectActivity.EXTRA_OBJECT, person)
                 startActivity(intentWithObject)
             }
+
+            R.id.btn_move_with_result -> {
+                val intentWithResult = Intent(this, IntentWithResultActivity::class.java)
+                startActivityForResult(intentWithResult, REQUEST_CODE)
+            }
         }
 
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQUEST_CODE) {
+            if (resultCode == IntentWithResultActivity.RESULT_CODE) {
+                val selectedValue = data?.getIntExtra(IntentWithResultActivity.EXTRA_SELECTED_VALUE, 0)
+                tvResult.text = "Hasil : $selectedValue"
+            }
+        }
     }
 }
--- app/src/main/java/com/example/fundamental/IntentWithResultActivity.kt
+++ app/src/main/java/com/example/fundamental/IntentWithResultActivity.kt
@@ -1,3 +1,49 @@
 package com.example.fundamental
 
-class IntentWithResultActivity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.RadioGroup
+
+class IntentWithResultActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var btnSelected: Button
+    private lateinit var rgNumber: RadioGroup
+
+    companion object {
+        const val EXTRA_SELECTED_VALUE = "extra_selected_value"
+        const val RESULT_CODE = 110
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intent_with_result)
+
+        btnSelected = findViewById(R.id.btn_choose)
+        rgNumber = findViewById(R.id.rg_number)
+
+        btnSelected.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        if (v?.id == R.id.btn_choose) {
+            if (rgNumber.checkedRadioButtonId > 0) {
+                var value = 0
+                when (rgNumber.checkedRadioButtonId) {
+                    R.id.rb_100 -> value = 100
+
+                    R.id.rb_200 -> value = 200
+
+                    R.id.rb_300 -> value = 300
+                }
+
+                val resultIntent = Intent()
+                resultIntent.putExtra(EXTRA_SELECTED_VALUE, value)
+                setResult(RESULT_CODE, resultIntent)
+                finish()
+            }
+        }
+    }
+}
