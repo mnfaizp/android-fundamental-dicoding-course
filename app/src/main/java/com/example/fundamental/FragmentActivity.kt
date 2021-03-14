126e2be8eb645a33c665960c17a26112e102356a
Znth3 <zynthkas@gmail.com>
2021-03-14 20:57:46 +0700

    first fragment

--- app/src/main/java/com/example/fundamental/FragmentActivity.kt
+++ app/src/main/java/com/example/fundamental/FragmentActivity.kt
@@ -2,10 +2,24 @@ package com.example.fundamental
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class FragmentActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fragment)
+
+        val mFragmentManager = supportFragmentManager
+        val mHomeFragment = HomeFragment()
+        val fragment = mFragmentManager.findFragmentByTag(HomeFragment::class.java.simpleName)
+
+        if (fragment !is HomeFragment) {
+            Log.d("My Flexible Fragment", "Fragment Name :" + HomeFragment::class.java.simpleName)
+
+            mFragmentManager
+                .beginTransaction()
+                .add(R.id.frame_container, mHomeFragment, HomeFragment::class.java.simpleName)
+                .commit()
+        }
     }
 }
