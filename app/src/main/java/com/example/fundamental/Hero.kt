89473d8677f9c1a28ca0fc5dfd9488a61cda158f
Znth3 <zynthkas@gmail.com>
2021-03-16 16:39:48 +0700

    last ListView

---
+++ app/src/main/java/com/example/fundamental/Hero.kt
@@ -0,0 +1,7 @@
+package com.example.fundamental
+
+data class Hero(
+    var photo: Int,
+    var name: String,
+    var desc: String
+)
--- app/src/main/java/com/example/fundamental/ListViewActivity.kt
+++ app/src/main/java/com/example/fundamental/ListViewActivity.kt
@@ -2,10 +2,26 @@ package com.example.fundamental
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
 class ListViewActivity : AppCompatActivity() {
+
+    private val dataName = arrayOf("Cut Nyak Dien",
+        "Ki Hajar Dewantara",
+        "Moh Yamin",
+        "Pattimura",
+        "R A Kartini",
+        "Sukarno")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_view)
+
+        val lvHeroes: ListView = findViewById(R.id.lv_heroes)
+        val adapter = ArrayAdapter<String>(this,
+            android.R.layout.simple_list_item_1, android.R.id.text1, dataName)
+        lvHeroes.adapter = adapter
+
     }
 }
