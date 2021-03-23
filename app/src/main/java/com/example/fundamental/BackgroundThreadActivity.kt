c9cd1ecc9b48baf771a70a87e1c3a2e5b9592c82
Znth3 <zynthkas@gmail.com>
2021-03-23 20:49:34 +0700

    generate new BackgroundThreadActivity.kt

---
+++ app/src/main/java/com/example/fundamental/BackgroundThreadActivity.kt
@@ -0,0 +1,11 @@
+package com.example.fundamental
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BackgroundThreadActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_background_thread)
+    }
+}
