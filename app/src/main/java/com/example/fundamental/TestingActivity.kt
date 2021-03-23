22ead2e03e3671932bdb2079a99a8637ddf3dc4e
Znth3 <zynthkas@gmail.com>
2021-03-23 12:56:16 +0700

    generate testing app

---
+++ app/src/main/java/com/example/fundamental/TestingActivity.kt
@@ -0,0 +1,11 @@
+package com.example.fundamental
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TestingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_testing)
+    }
+}
