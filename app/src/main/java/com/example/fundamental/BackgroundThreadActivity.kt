3f5034ca3af4358e9c56e8ab39408f32d1f41e7c
Znth3 <zynthkas@gmail.com>
2021-03-23 21:03:47 +0700

    executors & handler ini BackgroundThreadActivity.kt

--- app/src/main/java/com/example/fundamental/BackgroundThreadActivity.kt
+++ app/src/main/java/com/example/fundamental/BackgroundThreadActivity.kt
@@ -2,7 +2,11 @@ package com.example.fundamental
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import com.example.fundamental.databinding.ActivityBackgroundThreadBinding
+import java.util.concurrent.Executor
+import java.util.concurrent.Executors
 
 class BackgroundThreadActivity : AppCompatActivity() {
 
@@ -12,5 +16,30 @@ class BackgroundThreadActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityBackgroundThreadBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val executor = Executors.newSingleThreadExecutor()
+        val handler = Handler(Looper.getMainLooper())
+
+        binding.btnStart.setOnClickListener {
+            executor.execute {
+                try {
+                    for (i in 0..10) {
+                        Thread.sleep(500)
+                        val percentage = i * 10
+
+                        handler.post {
+                            if (percentage == 100) {
+                                binding.tvStatus.setText(R.string.task_completed)
+                            } else {
+                                binding.tvStatus.text =
+                                    String.format(getString(R.string.compressing), percentage)
+                            }
+                        }
+                    }
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
+                }
+            }
+        }
     }
 }
