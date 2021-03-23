08effe46cf0532a63131e889e7c9e9eae5659384
Znth3 <zynthkas@gmail.com>
2021-03-23 21:08:35 +0700

    coroutine uses in BackgroundThreadActivity.kt

--- app/src/main/java/com/example/fundamental/BackgroundThreadActivity.kt
+++ app/src/main/java/com/example/fundamental/BackgroundThreadActivity.kt
@@ -4,7 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import androidx.lifecycle.lifecycleScope
 import com.example.fundamental.databinding.ActivityBackgroundThreadBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.util.concurrent.Executor
 import java.util.concurrent.Executors
 
@@ -17,27 +22,21 @@ class BackgroundThreadActivity : AppCompatActivity() {
         binding = ActivityBackgroundThreadBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val executor = Executors.newSingleThreadExecutor()
-        val handler = Handler(Looper.getMainLooper())
-
         binding.btnStart.setOnClickListener {
-            executor.execute {
-                try {
-                    for (i in 0..10) {
-                        Thread.sleep(500)
-                        val percentage = i * 10
+            lifecycleScope.launch(Dispatchers.Default) {
+                for (i in 0..10) {
+                    delay(500)
+                    val percentage = i * 10
 
-                        handler.post {
-                            if (percentage == 100) {
-                                binding.tvStatus.setText(R.string.task_completed)
-                            } else {
-                                binding.tvStatus.text =
-                                    String.format(getString(R.string.compressing), percentage)
-                            }
+                    withContext(Dispatchers.Main)
+                    {
+                        if (percentage == 100) {
+                            binding.tvStatus.setText(R.string.task_completed)
+                        } else {
+                            binding.tvStatus.text =
+                                String.format(getString(R.string.compressing), percentage)
                         }
                     }
-                } catch (e: InterruptedException) {
-                    e.printStackTrace()
                 }
             }
         }
