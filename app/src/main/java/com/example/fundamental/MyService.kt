05ef7e6b908826f3b232234e39a0ada7868f51da
Znth3 <zynthkas@gmail.com>
2021-03-24 14:34:12 +0700

    complete service codelab

--- app/src/main/java/com/example/fundamental/MyService.kt
+++ app/src/main/java/com/example/fundamental/MyService.kt
@@ -5,15 +5,30 @@ import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.IBinder
 import android.util.Log
+import kotlinx.coroutines.*
 
 class MyService : Service() {
+
+    private val serviceJob = Job()
+    private val serviceScope = CoroutineScope(Dispatchers.Main + serviceJob)
 
     override fun onBind(intent: Intent): IBinder {
         throw UnsupportedOperationException("Not Yet Implemented")
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        Log.d(TAG, "Service dijalankan")
+        Log.d(TAG, "Service running..")
+        serviceScope.launch {
+            delay(3000)
+            stopSelf()
+            Log.d(TAG, "Service stopped..")
+        }
         return START_STICKY
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        serviceJob.cancel()
+        Log.d(TAG, "onDestroy: ")
     }
 }
