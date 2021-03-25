5de2dc52258095f19718d5e206c18762dc4b8487
Znth3 <zynthkas@gmail.com>
2021-03-25 22:30:11 +0700

    complete work manager

---
+++ app/src/main/java/com/example/fundamental/MyWorker.kt
@@ -0,0 +1,107 @@
+package com.example.fundamental
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import android.os.Looper
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.work.ListenableWorker
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+import com.loopj.android.http.AsyncHttpResponseHandler
+import com.loopj.android.http.SyncHttpClient
+import cz.msebera.android.httpclient.Header
+import org.json.JSONObject
+import java.lang.Exception
+import java.text.DecimalFormat
+
+class MyWorker(context: Context, workerParameters: WorkerParameters) : Worker(context, workerParameters) {
+
+    companion object {
+        private val TAG = MyWorker::class.java.simpleName
+        const val APP_ID = "caab4f03f4f4f0b87e0d69427f1f5a43"
+        const val EXTRA_CITY = "city"
+        const val NOTIFICATION_ID = 1
+        const val CHANNEL_ID = "channel_1"
+        const val CHANNEL_NAME = "dicoding_channel"
+    }
+
+    private var resultStatus: Result? = null
+
+    override fun doWork(): Result {
+        val dataCity = inputData.getString(EXTRA_CITY)
+        return getCurrentWeather(dataCity)
+    }
+
+    private fun getCurrentWeather(city: String?): Result {
+        Log.d(TAG, "getCurrentWeather: Start..")
+        Looper.prepare()
+        val client = SyncHttpClient()
+        val url = "https://api.openweathermap.org/data/2.5/weather?q=$city&appid=$APP_ID"
+        Log.d(TAG, "getCurrentWeather: $url")
+
+        client.get(url, object : AsyncHttpResponseHandler() {
+            override fun onSuccess(
+                statusCode: Int,
+                headers: Array<out Header>?,
+                responseBody: ByteArray?
+            ) {
+                val result = String(responseBody!!)
+                Log.d(TAG, result)
+
+                try {
+                    val responseObject = JSONObject(result)
+                    val currentWeather: String = responseObject.getJSONArray("weather").getJSONObject(0).getString("main")
+                    val description: String = responseObject.getJSONArray("weather").getJSONObject(0).getString("description")
+                    val tempInKelvin = responseObject.getJSONObject("main").getDouble("temp")
+                    val tempInCelsius = tempInKelvin - 273
+                    val temperature: String = DecimalFormat("##.##").format(tempInCelsius)
+                    val title = "Current Weather in $city"
+                    val message = "$currentWeather, $description with $temperature celsius"
+                    showNotification(title, message)
+                    Log.d(TAG, "onSuccess: Finish")
+
+                    resultStatus = Result.success()
+                } catch (e: Exception) {
+                    showNotification("Get Current Weather Not Success", e.message!!)
+                    Log.d(TAG, "OnSuccess: Fail")
+                    resultStatus = Result.failure()
+                }
+            }
+
+            override fun onFailure(
+                statusCode: Int,
+                headers: Array<out Header>?,
+                responseBody: ByteArray?,
+                error: Throwable?
+            ) {
+                Log.d(TAG, "onFailure: Fail")
+
+                showNotification("Get current weather data failed", error?.message!!)
+                resultStatus = Result.failure()
+            }
+        })
+
+        return resultStatus as Result
+    }
+
+    private fun showNotification(title: String, description: String) {
+        val notificationManager = applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val notification: NotificationCompat.Builder = NotificationCompat.Builder(applicationContext, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_baseline_clear_24)
+            .setContentTitle(title)
+            .setContentText(description)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setDefaults(NotificationCompat.DEFAULT_ALL)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_HIGH)
+            notification.setChannelId(CHANNEL_ID)
+            notificationManager.createNotificationChannel(channel)
+        }
+        notificationManager.notify(NOTIFICATION_ID, notification.build())
+    }
+
+}
--- app/src/main/java/com/example/fundamental/WorkManagerActivity.kt
+++ app/src/main/java/com/example/fundamental/WorkManagerActivity.kt
@@ -2,10 +2,90 @@ package com.example.fundamental
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.work.*
+import com.example.fundamental.databinding.ActivityWorkManagerBinding
+import java.util.concurrent.TimeUnit
 
-class WorkManagerActivity : AppCompatActivity() {
+class WorkManagerActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var workManager: WorkManager
+    private lateinit var periodicWorkRequest: PeriodicWorkRequest
+    private lateinit var binding: ActivityWorkManagerBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_work_manager)
+
+        binding = ActivityWorkManagerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        workManager = WorkManager.getInstance(this)
+        binding.btnOneTimeTask.setOnClickListener(this)
+        binding.btnPeriodicTask.setOnClickListener(this)
+        binding.btnCancelTask.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+
+        when (v?.id) {
+            R.id.btnOneTimeTask -> startOneTimeTask()
+            R.id.btnPeriodicTask -> startPeriodicTask()
+            R.id.btnCancelTask -> cancelPeriodicTask()
+        }
+    }
+
+    private fun cancelPeriodicTask() {
+        workManager.cancelWorkById(periodicWorkRequest.id)
+    }
+
+    private fun startPeriodicTask() {
+        binding.textStatus.text = getString(R.string.status)
+
+        val data = Data.Builder()
+            .putString(MyWorker.EXTRA_CITY, binding.editCity.text.toString())
+            .build()
+
+        val constrains = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .build()
+
+        val periodicWorkRequest = PeriodicWorkRequest.Builder(MyWorker::class.java, 15, TimeUnit.SECONDS)
+            .setInputData(data)
+            .build()
+
+        workManager.enqueue(periodicWorkRequest)
+        workManager.getWorkInfoByIdLiveData(periodicWorkRequest.id)
+            .observe(this, {
+                val status = it.state.name
+                binding.textStatus.append("\n" + status)
+                binding.btnCancelTask.isEnabled = false
+
+                if (it.state == WorkInfo.State.ENQUEUED) {
+                    binding.btnCancelTask.isEnabled = true
+                }
+            })
+    }
+
+    private fun startOneTimeTask() {
+        binding.textStatus.text = getString(R.string.status)
+
+        val data = Data.Builder()
+            .putString(MyWorker.EXTRA_CITY, binding.editCity.text.toString())
+            .build()
+
+        val constrains = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .build()
+
+        val oneTimeWorkRequest = OneTimeWorkRequest.Builder(MyWorker::class.java)
+            .setInputData(data)
+            .build()
+
+        workManager.enqueue(oneTimeWorkRequest)
+        workManager.getWorkInfoByIdLiveData(oneTimeWorkRequest.id)
+            .observe(this, {
+                val status = it.state.name
+                binding.textStatus.append("\n" + status)
+            })
     }
 }
